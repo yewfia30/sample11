0dc88a6eac4019cbf4e7cc7a6be6857a9b432716
yewfia30 <yewfia30@gmail.com>
2016-03-27 10:35:05 +0900

    Initial Commit

---
+++ sample11/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sample11
+//
+//  Created by yoshinobu on 2016/03/27.
+//  Copyright © 2016年 testorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sample11/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sample11
+//
+//  Created by yoshinobu on 2016/03/27.
+//  Copyright © 2016年 testorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
