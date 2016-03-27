f6de4866e95dda5752f53ce2380c45a0aa847246
yewfia30 <yewfia30@gmail.com>
2016-03-27 10:36:52 +0900

    first commit

--- sample11/ViewController.m
+++ sample11/ViewController.m
@@ -3,7 +3,7 @@
 //  sample11
 //
 //  Created by yoshinobu on 2016/03/27.
-//  Copyright © 2016年 testorg. All rights reserved.
+//  Copyright © 2016年 testorg. All rights reserved..
 //
 
 #import "ViewController.h"
